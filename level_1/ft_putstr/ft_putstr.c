d3db14bb88fdc67a4889392274cecdacc9413d66
avrgc0der <avrgbrowser@gmail.com>
2025-02-02 17:56:29 +0400

    just did first_word

---
+++ level_1/ft_putstr/ft_putstr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enoshahi <enoshahi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 11:35:58 by enoshahi          #+#    #+#             */
+/*   Updated: 2024/12/24 11:42:23 by enoshahi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(void)
+{
+	char *str;
+
+	str = "Hello";
+	ft_putstr(str);
+}
---
+++ level_1/ft_strcpy/ft_strcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enoshahi <enoshahi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/24 11:48:51 by enoshahi          #+#    #+#             */
+/*   Updated: 2024/12/24 11:50:03 by enoshahi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Reproduce the behavior of the function strcpy (man strcpy).
+
+// Your function must be declared as follows:
+
+// char    *ft_strcpy(char *s1, char *s2);
+
+char    *ft_strcpy(char *s1, char *s2);
+{
+	int i;
+
+	i = 0;
+	while(s1[i])
+	{
+		
+	}
+}
+
+int main(void)
